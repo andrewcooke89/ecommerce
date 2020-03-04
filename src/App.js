4716d3d0520f09e3ba48b959f9b2fe00bfa7bc0a
Andrew Cooke <cookecodes89@gmail.com>
2020-03-04 14:25:32 +0100

    routing done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from "./pages/homepage/homepage.component";
+
+const TopicsList = () => {
+  return (
+    <div>
+      <Link to='/topics' />
+      <h1>Topic List</h1>
+    </div>
+  );
+};
+
+const TopicDetail = (props) => {
+  return (
+    <div>
+      <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/topics" component={TopicsList} />
+        <Route path="/topics/:topicId" component={TopicDetail} />
+      </Switch>
     </div>
   );
 }
